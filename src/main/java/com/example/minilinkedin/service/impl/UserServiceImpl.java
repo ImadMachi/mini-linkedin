45f760be076c19a91add42ba5f78ad9a9d6a8c97
Imad Machi <machii.imad@gmail.com>
2022-04-08 11:56:51 +0000

    modify user service

--- src/main/java/com/example/minilinkedin/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/minilinkedin/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
-package com.example.minilinkedin.service;
+package com.example.minilinkedin.service.impl;
 
+import com.example.minilinkedin.bean.ReponseAnnonce;
 import com.example.minilinkedin.bean.User;
 import com.example.minilinkedin.dao.UserDao;
 import com.example.minilinkedin.service.facade.UserService;
@@ -7,8 +8,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService {
+    public int save(User user) {
+        User existingUser = userDao.findByLogin(user.getLogin());
+        if(existingUser != null) {
+            return -1;
+        } else {
+            userDao.save(user);
+            return 1;
+        }
+    }
+
     public User findByLogin(String login) {
         return userDao.findByLogin(login);
     }
@@ -16,6 +29,8 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public int deleteByLogin(String login){
         User user = userDao.findByLogin(login);
+
+
         if(user == null) {
             return -1;
         } else {
