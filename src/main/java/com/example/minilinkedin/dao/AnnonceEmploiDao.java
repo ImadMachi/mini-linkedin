5a40c8314c89f9d27a8ac115eeb43ba63f426f2a
ibtissamelhani <ibtissamelhani36@gmail.com>
2022-04-08 11:03:55 +0000

    add bean

---
+++ src/main/java/com/example/minilinkedin/dao/AnnonceEmploiDao.java
@@ -0,0 +1,11 @@
+package com.example.minilinkedin.dao;
+
+import com.example.minilinkedin.bean.AnnonceEmploi;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnnonceEmploiDao extends JpaRepository<AnnonceEmploi,Long> {
+    AnnonceEmploi findByRef(String ref);
+
+}
