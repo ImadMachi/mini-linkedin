b40fd2116c7c5b5ba26b6eaf136fb15b64fb65e7
Imad Machi <machii.imad@gmail.com>
2022-04-08 13:46:31 +0000

    add webservice

--- src/main/java/com/example/minilinkedin/ws/UserRest.java
+++ src/main/java/com/example/minilinkedin/ws/UserRest.java
@@ -1,10 +1,21 @@
 package com.example.minilinkedin.ws;
 
+import com.example.minilinkedin.bean.User;
+import com.example.minilinkedin.service.facade.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/commande")
+@RequestMapping("/api/v1/user")
 public class UserRest {
+    @PostMapping("/")
+    public int exec(@RequestBody User user) {
+        return userService.exec(user);
+    }
 
+    @Autowired
+    UserService userService;
 }
