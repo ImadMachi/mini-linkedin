83d9c1f4ffd820e6a02e40e236b05b058612b340
elidrissimohamed <med.elidrissi2000@gmail.com>
2022-04-08 13:59:34 +0200

    comp service v1

--- src/main/java/com/example/minilinkedin/service/facade/CompetenceService.java
+++ src/main/java/com/example/minilinkedin/service/facade/CompetenceService.java
@@ -1,4 +1,13 @@
 package com.example.minilinkedin.service.facade;
 
+import com.example.minilinkedin.bean.Competence;
+
+import java.util.List;
+
 public interface CompetenceService {
+    int save(Competence competence);
+    Competence findByLibelle(String libelle);
+    int deleteByLibelle(String libelle);
+    List<Competence> findByUserLogin(String login);
+    int deleteByUserLogin(String login);
 }
