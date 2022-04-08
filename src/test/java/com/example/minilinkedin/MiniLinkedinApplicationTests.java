9f39a93b50198478fd7e3834d129ed3dbd663885
Imad Machi <machii.imad@gmail.com>
2022-04-08 09:20:44 +0000

    first commit

---
+++ src/test/java/com/example/minilinkedin/MiniLinkedinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minilinkedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniLinkedinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
