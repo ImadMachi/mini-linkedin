a75f2ba4c9f036c4f25dc4ebeda1486543429b69
elidrissimohamed <med.elidrissi2000@gmail.com>
2022-04-08 13:01:49 +0200

    bean

---
+++ src/main/java/com/example/minilinkedin/dao/CompetenceDao.java
@@ -0,0 +1,12 @@
+package com.example.minilinkedin.dao;
+
+import com.example.minilinkedin.bean.Competence;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompetenceDao extends JpaRepository<Competence,Long> {
+    Competence findByLibelle(String libelle) ;
+
+
+}
