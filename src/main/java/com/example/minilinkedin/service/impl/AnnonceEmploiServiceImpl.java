941a30fc773f7d59d63bafcd815d14f5534d97a1
ibtissamelhani <ibtissamelhani36@gmail.com>
2022-04-08 11:49:31 +0000

    add bean

--- src/main/java/com/example/minilinkedin/service/impl/AnnonceEmploiServiceImpl.java
+++ src/main/java/com/example/minilinkedin/service/impl/AnnonceEmploiServiceImpl.java
@@ -5,11 +5,21 @@ import com.example.minilinkedin.dao.AnnonceEmploiDao;
 import com.example.minilinkedin.service.facade.AnnonceEmploiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AnnonceEmploiServiceImpl implements AnnonceEmploiService {
 
-
+    @Transactional
+    int deleteAnnonce(String ref){
+        AnnonceEmploi annonce = annonceEmploiDao.findByRef(ref);
+        if (annonce == null){
+            return -1;
+        }else{
+            annonceEmploiDao.deleteByRef(ref);
+            return 1;
+        }
+    }
 
     public AnnonceEmploi findByRef(String ref) {
         return annonceEmploiDao.findByRef(ref);
