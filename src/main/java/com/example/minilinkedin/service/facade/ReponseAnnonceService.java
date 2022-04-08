a319453e35dd7dc8a6359fa9c22b7d77df9d03f2
AichaAitElbaz <aichaelbaz191220@gmail.com>
2022-04-08 14:42:49 +0200

    test

--- src/main/java/com/example/minilinkedin/service/facade/ReponseAnnonceService.java
+++ src/main/java/com/example/minilinkedin/service/facade/ReponseAnnonceService.java
@@ -1,5 +1,10 @@
 package com.example.minilinkedin.service.facade;
 
+import com.example.minilinkedin.bean.ReponseAnnonce;
+
 public interface ReponseAnnonceService {
+     int save(ReponseAnnonce reponseAnnonce);
+
+
 
 }
--- src/main/java/com/example/minilinkedin/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/minilinkedin/service/impl/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.example.minilinkedin.service.impl;
 
+import com.example.minilinkedin.bean.Competence;
 import com.example.minilinkedin.bean.ReponseAnnonce;
 import com.example.minilinkedin.bean.User;
 import com.example.minilinkedin.dao.UserDao;
+import com.example.minilinkedin.service.facade.CompetenceService;
+import com.example.minilinkedin.service.facade.ReponseAnnonceService;
 import com.example.minilinkedin.service.facade.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,21 +15,38 @@ import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
-   int validate(User user){
-if (user.getLogin()==null)return -1;
-else if (user.getCompetences()==null||user.getCompetences().isEmpty())return -2;
-else if (user.getReponseAnnonces()==null||user.getReponseAnnonces().isEmpty())return -3;
-else return 1;
+    private int validate(User user) {
+        if (user.getLogin() == null) return -1;
+        else if (user.getCompetences() == null || user.getCompetences().isEmpty()) return -2;
+        else if (user.getReponseAnnonces() == null || user.getReponseAnnonces().isEmpty()) return -3;
+        else return 1;
     }
-    
-    public int save(User user) {
-        User existingUser = userDao.findByLogin(user.getLogin());
-        if(existingUser != null) {
-            return -1;
-        } else {
-            userDao.save(user);
-            return 1;
+
+    private void hundleProcess(User user) {
+        userDao.save(user);
+        saveCompetences(user);
+        saveReponseAnonces(user);
+
+    }
+
+    private void saveCompetences(User user) {
+        for (Competence c : user.getCompetences()) {
+            c.setUser(user);
+            competenceService.save(c);
         }
+    }
+
+    private void saveReponseAnonces(User user) {
+        for (ReponseAnnonce rep : user.getReponseAnnonces()) {
+            rep.setUser(user);
+            reponseAnnonceService.save(rep);
+        }
+    }
+
+    int exec(User user) {
+        int res = validate(user);
+        if (res > 0) hundleProcess(user);
+        return res;
     }
 
     public User findByLogin(String login) {
@@ -34,17 +54,26 @@ else return 1;
     }
 
     @Transactional
-    public int deleteByLogin(String login){
+    public int deleteByLogin(String login) {
         User user = userDao.findByLogin(login);
-        if(user == null) {
+        if (user == null) {
             return -1;
         } else {
-            user.getReponseAnnonces().forEach(e-> reponseAnnonceServiceImpl.deleteByUserLogin(e.getUser().getLogin()));
+            user.getReponseAnnonces().forEach(e -> reponseAnnonceServiceImpl.deleteByUserLogin(e.getUser().getLogin()));
             userDao.deleteByLogin(login);
             return 1;
         }
     }
 
+    @Override
+    public int save(User user) {
+        return 0;
+    }
+
+    @Autowired
+    private ReponseAnnonceService reponseAnnonceService;
+    @Autowired
+    private CompetenceService competenceService;
     @Autowired
     private UserDao userDao;
 
